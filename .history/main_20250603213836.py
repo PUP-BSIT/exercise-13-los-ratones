5b7f2203f06a9a7286a2582922f7a329082a0c9d
reduta-paulbenidict <paulbenidictr@gmail.com>
2025-06-03 21:49:08 +0800

    add main functionality for exercise 13.

---
+++ .history/main_20250603213836.py
@@ -0,0 +1,9 @@
+
+
+while True:
+    print("Welcome to Los Ratones!!"
+          + "\n1. "
+          + "\n2. "
+          + "\n3. "
+          + "\n4. "
+          + "\n5. ")
---
+++ .history/main_20250603213958.py
@@ -0,0 +1,12 @@
+
+
+while True:
+    print("Welcome to Los Ratones!!"
+          + "\n1. "
+          + "\n2. "
+          + "\n3. "
+          + "\n4. "
+          + "\n5. "
+          + "\n6. Exit")
+    
+    
